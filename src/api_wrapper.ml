9ff47f59f79269f362bdf358fcc81e8e00d39a93
Levi Roth <levimroth@gmail.com>
2019-08-24 10:43:13 -0400

    Provide deferred and non-deferred versions of Api_wrapper.to_body_json

--- src/api_wrapper.ml
+++ src/api_wrapper.ml
@@ -1,11 +1,12 @@
 open! Core
 open Async
 
-let to_body_json deferred =
-  let%bind response, body = deferred in
+let to_body_json (response, body) =
   match Cohttp.Response.status response with
   | #Cohttp.Code.success_status ->
     let%bind body_s = Cohttp_async.Body.to_string body in
     Yojson.Safe.from_string body_s |> Ok |> return
   | _ -> return (Error (response, body))
 ;;
+
+let to_body_json' = Deferred.bind ~f:to_body_json
