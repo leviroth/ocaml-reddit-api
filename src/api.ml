129796144e87fb65ee6c8aa500dd29c9d67b7094
Levi Roth <levimroth@gmail.com>
2019-08-31 08:18:40 -0400

    Move Api parameters back into Api module

--- src/api.ml
+++ src/api.ml
@@ -1,6 +1,495 @@
 open! Core
 open! Async
-include Api_parameters
+
+module Parameters = struct
+  module Pagination = struct
+    type t =
+      | Before of Fullname.t
+      | After of Fullname.t
+    [@@deriving sexp]
+
+    let params_of_t t =
+      let key =
+        match t with
+        | Before _ -> "before"
+        | After _ -> "after"
+      in
+      let value =
+        match t with
+        | Before fullname | After fullname -> Fullname.to_string fullname
+      in
+      [ key, [ value ] ]
+    ;;
+  end
+
+  module Comment_sort = struct
+    type t =
+      | Confidence
+      | Top
+      | New
+      | Controversial
+      | Old
+      | Random
+      | Q_and_a
+      | Live
+    [@@deriving sexp]
+
+    let to_string t =
+      match t with
+      | Q_and_a -> "qa"
+      | _ -> sexp_of_t t |> Sexp.to_string |> String.lowercase
+    ;;
+  end
+
+  module Report_target = struct
+    type t =
+      | Modmail_conversation of Id36.t
+      | Fullname of Fullname.t
+    [@@deriving sexp]
+
+    let params_of_t t =
+      match t with
+      | Modmail_conversation id -> [ "modmail_conv_id", [ Id36.to_string id ] ]
+      | Fullname fullname -> [ "thing_id", [ Fullname.to_string fullname ] ]
+    ;;
+  end
+
+  module Sticky_state = struct
+    type t =
+      | Sticky of { slot : int }
+      | Unsticky
+    [@@deriving sexp]
+
+    let params_of_t t =
+      match t with
+      | Sticky { slot } ->
+        [ "state", [ Bool.to_string true ]; "num", [ Int.to_string slot ] ]
+      | Unsticky -> [ "state", [ Bool.to_string false ] ]
+    ;;
+  end
+
+  module Submission_kind = struct
+    module Self_post_body = struct
+      type t =
+        | Markdown of string
+        | Richtext_json of Json_derivers.Yojson.t
+      [@@deriving sexp]
+    end
+
+    type t =
+      | Link of { url : string }
+      | Self of Self_post_body.t
+      | Image
+      | Video
+      | Videogif
+    [@@deriving sexp]
+
+    let params_of_t t =
+      match t with
+      | Link { url } -> [ "kind", [ "link" ]; "url", [ url ] ]
+      | Self body ->
+        [ "kind", [ "link" ]
+        ; ( "text"
+          , [ (match body with
+              | Markdown markdown -> markdown
+              | Richtext_json json -> Yojson.Safe.to_string json)
+            ] )
+        ]
+      (* TODO Should we disallow these? *)
+      | Image | Video | Videogif -> assert false
+    ;;
+  end
+
+  module Vote_direction = struct
+    type t =
+      | Up
+      | Neutral
+      | Down
+    [@@deriving sexp]
+
+    let int_of_t t =
+      match t with
+      | Up -> 1
+      | Neutral -> 0
+      | Down -> -1
+    ;;
+
+    let params_of_t t = [ "dir", [ int_of_t t |> Int.to_string ] ]
+  end
+
+  module Info_query = struct
+    type t =
+      | Id of Fullname.t list
+      | Url of Uri_sexp.t
+    [@@deriving sexp]
+
+    let params_of_t t =
+      match t with
+      | Id fullnames -> [ "id", List.map fullnames ~f:Fullname.to_string ]
+      | Url uri -> [ "url", [ Uri.to_string uri ] ]
+    ;;
+  end
+
+  module Duplicate_sort = struct
+    type t =
+      | Number_of_comments
+      | New
+
+    let params_of_t t =
+      [ ( "sort"
+        , match t with
+          | Number_of_comments -> [ "number_of_comments" ]
+          | New -> [ "new" ] )
+      ]
+    ;;
+  end
+
+  module Historical_span = struct
+    module T = struct
+      type t =
+        | Hour
+        | Day
+        | Week
+        | Month
+        | Year
+        | All
+      [@@deriving sexp]
+    end
+
+    include T
+    include Sexpable.To_stringable (T)
+
+    let params_of_t t = [ "t", [ to_string t |> String.lowercase ] ]
+  end
+
+  module Mod_filter = struct
+    type t =
+      | Moderators of Username.t list
+      | Admin
+
+    let params_of_t t =
+      [ ( "mod"
+        , match t with
+          | Admin -> [ "a" ]
+          | Moderators moderators -> List.map moderators ~f:Username.to_string )
+      ]
+    ;;
+  end
+
+  module Links_or_comments = struct
+    type t =
+      | Links
+      | Comments
+
+    let params_of_t t =
+      [ ( "only"
+        , match t with
+          | Links -> [ "links" ]
+          | Comments -> [ "comments" ] )
+      ]
+    ;;
+  end
+
+  module How_to_distinguish = struct
+    type t =
+      | Mod
+      | Admin
+      | Special
+      | Undistinguish
+
+    let params_of_t t =
+      [ ( "how"
+        , [ (match t with
+            | Mod -> "yes"
+            | Admin -> "admin"
+            | Special -> "special"
+            | Undistinguish -> "no")
+          ] )
+      ]
+    ;;
+  end
+
+  module Search_sort = struct
+    type t =
+      | Relevance
+      | Hot
+      | Top
+      | New
+      | Comments
+
+    let params_of_t t =
+      [ ( "sort"
+        , [ (match t with
+            | Relevance -> "relevance"
+            | Hot -> "hot"
+            | Top -> "top"
+            | New -> "new"
+            | Comments -> "comments")
+          ] )
+      ]
+    ;;
+  end
+
+  module Search_type = struct
+    module T = struct
+      type t =
+        | Subreddit
+        | Submission
+        | User
+      [@@deriving compare, sexp]
+    end
+
+    include T
+    include Comparable.Make (T)
+
+    let to_string t =
+      match t with
+      | Subreddit -> "sr"
+      | Submission -> "link"
+      | User -> "user"
+    ;;
+  end
+
+  module Link_type = struct
+    type t =
+      | Any
+      | Link
+      | Self
+
+    let params_of_t t =
+      [ ( "link_type"
+        , [ (match t with
+            | Any -> "any"
+            | Link -> "link"
+            | Self -> "self")
+          ] )
+      ]
+    ;;
+  end
+
+  module Spam_level = struct
+    type t =
+      | Low
+      | High
+      | All
+
+    let to_string t =
+      match t with
+      | Low -> "low"
+      | High -> "high"
+      | All -> "all"
+    ;;
+  end
+
+  module Subreddit_type = struct
+    type t =
+      | Gold_restricted
+      | Archived
+      | Restricted
+      | Employees_only
+      | Gold_only
+      | Private
+      | User
+      | Public
+
+    let to_string t =
+      match t with
+      | Gold_restricted -> "gold_restricted"
+      | Archived -> "archived"
+      | Restricted -> "restricted"
+      | Employees_only -> "employees_only"
+      | Gold_only -> "gold_only"
+      | Private -> "private"
+      | User -> "user"
+      | Public -> "public"
+    ;;
+  end
+
+  module Wiki_mode = struct
+    type t =
+      | Disabled
+      | Mod_only
+      | Anyone
+
+    let to_string t =
+      match t with
+      | Disabled -> "disabled"
+      | Mod_only -> "modonly"
+      | Anyone -> "anyone"
+    ;;
+  end
+
+  module Stylesheet_operation = struct
+    type t =
+      | Save
+      | Preview
+    [@@deriving sexp]
+
+    let to_string t =
+      match t with
+      | Save -> "save"
+      | Preview -> "preview"
+    ;;
+  end
+
+  module Subscription_action = struct
+    type t =
+      | Subscribe
+      | Unsubscribe
+
+    let to_string t =
+      match t with
+      | Subscribe -> "sub"
+      | Unsubscribe -> "unsub"
+    ;;
+  end
+
+  module Image_type = struct
+    type t =
+      | Png
+      | Jpg
+
+    let to_string t =
+      match t with
+      | Png -> "png"
+      | Jpg -> "jpg"
+    ;;
+  end
+
+  module Upload_type = struct
+    type t =
+      | Image
+      | Header
+      | Icon
+      | Banner
+
+    let to_string t =
+      match t with
+      | Image -> "img"
+      | Header -> "header"
+      | Icon -> "icon"
+      | Banner -> "banner"
+    ;;
+  end
+
+  module Subreddit_search_sort = struct
+    type t =
+      | Relevance
+      | Activity
+
+    let to_string t =
+      match t with
+      | Relevance -> "relevance"
+      | Activity -> "activity"
+    ;;
+  end
+
+  module Subreddit_relationship = struct
+    type t =
+      | Subscriber
+      | Contributor
+      | Moderator
+      | Stream_subscriber
+
+    let to_string t =
+      match t with
+      | Subscriber -> "subscriber"
+      | Contributor -> "contributor"
+      | Moderator -> "moderator"
+      | Stream_subscriber -> "streams"
+    ;;
+  end
+
+  module Subreddit_listing_sort = struct
+    type t =
+      | Popular
+      | New
+      | Gold
+      | Default
+
+    let to_string t =
+      match t with
+      | Popular -> "popular"
+      | New -> "new"
+      | Gold -> "gold"
+      | Default -> "default"
+    ;;
+  end
+
+  module User_subreddit_sort = struct
+    type t =
+      | Popular
+      | New
+
+    let to_string t =
+      match t with
+      | Popular -> "popular"
+      | New -> "new"
+    ;;
+  end
+
+  module Relationship = struct
+    module Duration = struct
+      type t =
+        | Permanent
+        | Days of int
+      [@@deriving sexp]
+
+      let params_of_t t =
+        [ ( "duration"
+          , match t with
+            | Permanent -> []
+            | Days days -> [ Int.to_string days ] )
+        ]
+      ;;
+    end
+
+    type t =
+      | Friend
+      | Moderator
+      | Moderator_invite
+      | Contributor
+      | Banned
+      | Muted
+      | Wiki_banned
+      | Wiki_contributor
+    [@@deriving sexp]
+
+    let to_string t =
+      match t with
+      | Friend -> "friend"
+      | Moderator -> "moderator"
+      | Moderator_invite -> "moderator_invite"
+      | Contributor -> "contributor"
+      | Banned -> "banned"
+      | Muted -> "muted"
+      | Wiki_banned -> "wikibanned"
+      | Wiki_contributor -> "wikicontributor"
+    ;;
+
+    let params_of_t t = [ "type", [ to_string t ] ]
+  end
+
+  module Wiki_page = struct
+    type t =
+      { subreddit : Subreddit_name.t
+      ; page : string
+      }
+    [@@deriving sexp]
+  end
+
+  module Add_or_remove = struct
+    type t =
+      | Add
+      | Remove
+
+    let to_string t =
+      match t with
+      | Add -> "add"
+      | Remove -> "del"
+    ;;
+  end
+end
 
 let call_api k ?(param_list_override = Fn.id) connection ~endpoint ~http_verb ~params =
   let params = ("raw_json", [ "1" ]) :: params in
@@ -69,6 +558,8 @@ let simple_toggle' verb k =
 let api_type : Param_dsl.t = [ "api_type", [ "json" ] ]
 
 module With_continuations = struct
+  open Parameters
+
   let me = get ~endpoint:"/api/v1/me" ~params:[]
   let karma = get ~endpoint:"/api/v1/me/karma" ~params:[]
   let trophies = get ~endpoint:"/api/v1/me/trophies" ~params:[]
@@ -1340,3 +1831,6 @@ module Raw = struct
 end
 
 include Raw
+
+type 'a listing =
+  ?pagination:Parameters.Pagination.t -> ?count:int -> ?limit:int -> ?show_all:unit -> 'a
--- src/api_intf.ml
+++ src/api_intf.ml
@@ -1,15 +1,257 @@
 open! Core
 open Async
-open Api_parameters
-
-type 'a listing =
-  ?pagination:Api_parameters.Pagination.t
-  -> ?count:int
-  -> ?limit:int
-  -> ?show_all:unit
-  -> 'a
 
 module type S = sig
+  module Parameters : sig
+    module Pagination : sig
+      type t =
+        | Before of Fullname.t
+        | After of Fullname.t
+      [@@deriving sexp]
+    end
+
+    module Comment_sort : sig
+      type t =
+        | Confidence
+        | Top
+        | New
+        | Controversial
+        | Old
+        | Random
+        | Q_and_a
+        | Live
+      [@@deriving sexp]
+    end
+
+    module Report_target : sig
+      type t =
+        | Modmail_conversation of Id36.t
+        | Fullname of Fullname.t
+      [@@deriving sexp]
+    end
+
+    module Sticky_state : sig
+      type t =
+        | Sticky of { slot : int }
+        | Unsticky
+      [@@deriving sexp]
+    end
+
+    module Submission_kind : sig
+      module Self_post_body : sig
+        type t =
+          | Markdown of string
+          | Richtext_json of Json_derivers.Yojson.t
+        [@@deriving sexp]
+      end
+
+      type t =
+        | Link of { url : string }
+        | Self of Self_post_body.t
+        | Image
+        | Video
+        | Videogif
+      [@@deriving sexp]
+    end
+
+    module Vote_direction : sig
+      type t =
+        | Up
+        | Neutral
+        | Down
+      [@@deriving sexp]
+    end
+
+    module Info_query : sig
+      type t =
+        | Id of Fullname.t list
+        | Url of Uri.t
+      [@@deriving sexp]
+    end
+
+    module Duplicate_sort : sig
+      type t =
+        | Number_of_comments
+        | New
+    end
+
+    module Historical_span : sig
+      type t =
+        | Hour
+        | Day
+        | Week
+        | Month
+        | Year
+        | All
+      [@@deriving sexp]
+    end
+
+    module Mod_filter : sig
+      type t =
+        | Moderators of Username.t list
+        | Admin
+    end
+
+    module Links_or_comments : sig
+      type t =
+        | Links
+        | Comments
+    end
+
+    module How_to_distinguish : sig
+      type t =
+        | Mod
+        | Admin
+        | Special
+        | Undistinguish
+    end
+
+    module Search_sort : sig
+      type t =
+        | Relevance
+        | Hot
+        | Top
+        | New
+        | Comments
+    end
+
+    module Search_type : sig
+      type t =
+        | Subreddit
+        | Submission
+        | User
+      [@@deriving sexp]
+
+      include Comparable.S with type t := t
+    end
+
+    module Link_type : sig
+      type t =
+        | Any
+        | Link
+        | Self
+    end
+
+    module Spam_level : sig
+      type t =
+        | Low
+        | High
+        | All
+    end
+
+    module Subreddit_type : sig
+      type t =
+        | Gold_restricted
+        | Archived
+        | Restricted
+        | Employees_only
+        | Gold_only
+        | Private
+        | User
+        | Public
+    end
+
+    module Wiki_mode : sig
+      type t =
+        | Disabled
+        | Mod_only
+        | Anyone
+    end
+
+    module Stylesheet_operation : sig
+      type t =
+        | Save
+        | Preview
+    end
+
+    module Subscription_action : sig
+      type t =
+        | Subscribe
+        | Unsubscribe
+    end
+
+    module Image_type : sig
+      type t =
+        | Png
+        | Jpg
+    end
+
+    module Upload_type : sig
+      type t =
+        | Image
+        | Header
+        | Icon
+        | Banner
+    end
+
+    module Subreddit_search_sort : sig
+      type t =
+        | Relevance
+        | Activity
+    end
+
+    module Subreddit_relationship : sig
+      type t =
+        | Subscriber
+        | Contributor
+        | Moderator
+        | Stream_subscriber
+    end
+
+    module Subreddit_listing_sort : sig
+      type t =
+        | Popular
+        | New
+        | Gold
+        | Default
+    end
+
+    module User_subreddit_sort : sig
+      type t =
+        | Popular
+        | New
+    end
+
+    module Relationship : sig
+      module Duration : sig
+        type t =
+          | Permanent
+          | Days of int
+        [@@deriving sexp]
+      end
+
+      type t =
+        | Friend
+        | Moderator
+        | Moderator_invite
+        | Contributor
+        | Banned
+        | Muted
+        | Wiki_banned
+        | Wiki_contributor
+      [@@deriving sexp]
+    end
+
+    module Wiki_page : sig
+      type t =
+        { subreddit : Subreddit_name.t
+        ; page : string
+        }
+      [@@deriving sexp]
+    end
+
+    module Add_or_remove : sig
+      type t =
+        | Add
+        | Remove
+    end
+  end
+
+  open Parameters
+
+  type 'a listing =
+    ?pagination:Pagination.t -> ?count:int -> ?limit:int -> ?show_all:unit -> 'a
+
   type 'a call
 
   (** Account *)
@@ -721,254 +963,7 @@ module type S = sig
     -> (Cohttp.Response.t * Cohttp_async.Body.t) call
 end
 
-module type Parameters = sig
-  module Pagination : sig
-    type t =
-      | Before of Fullname.t
-      | After of Fullname.t
-    [@@deriving sexp]
-  end
-
-  module Comment_sort : sig
-    type t =
-      | Confidence
-      | Top
-      | New
-      | Controversial
-      | Old
-      | Random
-      | Q_and_a
-      | Live
-    [@@deriving sexp]
-  end
-
-  module Report_target : sig
-    type t =
-      | Modmail_conversation of Id36.t
-      | Fullname of Fullname.t
-    [@@deriving sexp]
-  end
-
-  module Sticky_state : sig
-    type t =
-      | Sticky of { slot : int }
-      | Unsticky
-    [@@deriving sexp]
-  end
-
-  module Submission_kind : sig
-    module Self_post_body : sig
-      type t =
-        | Markdown of string
-        | Richtext_json of Json_derivers.Yojson.t
-      [@@deriving sexp]
-    end
-
-    type t =
-      | Link of { url : string }
-      | Self of Self_post_body.t
-      | Image
-      | Video
-      | Videogif
-    [@@deriving sexp]
-  end
-
-  module Vote_direction : sig
-    type t =
-      | Up
-      | Neutral
-      | Down
-    [@@deriving sexp]
-  end
-
-  module Info_query : sig
-    type t =
-      | Id of Fullname.t list
-      | Url of Uri.t
-    [@@deriving sexp]
-  end
-
-  module Duplicate_sort : sig
-    type t =
-      | Number_of_comments
-      | New
-  end
-
-  module Historical_span : sig
-    type t =
-      | Hour
-      | Day
-      | Week
-      | Month
-      | Year
-      | All
-    [@@deriving sexp]
-  end
-
-  module Mod_filter : sig
-    type t =
-      | Moderators of Username.t list
-      | Admin
-  end
-
-  module Links_or_comments : sig
-    type t =
-      | Links
-      | Comments
-  end
-
-  module How_to_distinguish : sig
-    type t =
-      | Mod
-      | Admin
-      | Special
-      | Undistinguish
-  end
-
-  module Search_sort : sig
-    type t =
-      | Relevance
-      | Hot
-      | Top
-      | New
-      | Comments
-  end
-
-  module Search_type : sig
-    type t =
-      | Subreddit
-      | Submission
-      | User
-    [@@deriving sexp]
-
-    include Comparable.S with type t := t
-  end
-
-  module Link_type : sig
-    type t =
-      | Any
-      | Link
-      | Self
-  end
-
-  module Spam_level : sig
-    type t =
-      | Low
-      | High
-      | All
-  end
-
-  module Subreddit_type : sig
-    type t =
-      | Gold_restricted
-      | Archived
-      | Restricted
-      | Employees_only
-      | Gold_only
-      | Private
-      | User
-      | Public
-  end
-
-  module Wiki_mode : sig
-    type t =
-      | Disabled
-      | Mod_only
-      | Anyone
-  end
-
-  module Stylesheet_operation : sig
-    type t =
-      | Save
-      | Preview
-  end
-
-  module Subscription_action : sig
-    type t =
-      | Subscribe
-      | Unsubscribe
-  end
-
-  module Image_type : sig
-    type t =
-      | Png
-      | Jpg
-  end
-
-  module Upload_type : sig
-    type t =
-      | Image
-      | Header
-      | Icon
-      | Banner
-  end
-
-  module Subreddit_search_sort : sig
-    type t =
-      | Relevance
-      | Activity
-  end
-
-  module Subreddit_relationship : sig
-    type t =
-      | Subscriber
-      | Contributor
-      | Moderator
-      | Stream_subscriber
-  end
-
-  module Subreddit_listing_sort : sig
-    type t =
-      | Popular
-      | New
-      | Gold
-      | Default
-  end
-
-  module User_subreddit_sort : sig
-    type t =
-      | Popular
-      | New
-  end
-
-  module Relationship : sig
-    module Duration : sig
-      type t =
-        | Permanent
-        | Days of int
-      [@@deriving sexp]
-    end
-
-    type t =
-      | Friend
-      | Moderator
-      | Moderator_invite
-      | Contributor
-      | Banned
-      | Muted
-      | Wiki_banned
-      | Wiki_contributor
-    [@@deriving sexp]
-  end
-
-  module Wiki_page : sig
-    type t =
-      { subreddit : Subreddit_name.t
-      ; page : string
-      }
-    [@@deriving sexp]
-  end
-
-  module Add_or_remove : sig
-    type t =
-      | Add
-      | Remove
-  end
-end
-
 module type Api = sig
-  include Parameters
-
   include
     S
       with type 'a call :=
@@ -979,6 +974,8 @@ module type Api = sig
 
   module Raw :
     S
+      with module Parameters := Parameters
+      with type 'a listing := 'a listing
       with type _ call :=
             ?param_list_override:((string * string list) list
                                   -> (string * string list) list)
