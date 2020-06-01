63a99ada4e24f6591b194eb9472dac841a1a34a0
Levi Roth <levimroth@gmail.com>
2020-05-31 22:07:38 -0400

    Add [Stream.fold] variations

--- src/stream.ml
+++ src/stream.ml
@@ -16,17 +16,33 @@ end = struct
   let after = Clock_ns.after
 end
 
-let iter
+open Continue_or_stop
+
+let fold_helper l ~init ~f =
+  let rec loop l ~state ~f =
+    match l with
+    | [] -> return (Continue state)
+    | hd :: tl ->
+      (match%bind f state hd with
+      | Stop final -> return (Stop final)
+      | Continue state -> loop tl ~state ~f)
+  in
+  loop l ~state:init ~f
+;;
+
+let fold_until_finished
     (type id)
     (module Id : Hashable.S with type t = id)
     connection
     ~get_listing
     ~get_before_parameter
+    ~init
     ~f
+    ~on_error
   =
   let module Bounded_set = Bounded_set.Make (Id) in
   let seen = Bounded_set.create ~capacity:300 in
-  let rec loop ?(first_pass = false) before backoff cache_busting_counter =
+  let rec loop ?(first_pass = false) state before backoff cache_busting_counter =
     let limit, cache_busting_counter =
       match before with
       | Some _ -> 100, cache_busting_counter
@@ -35,38 +51,88 @@ let iter
         100 - cache_busting_counter, cache_busting_counter
     in
     match%bind get_listing connection ~before ~limit with
-    | Error (response, (_ : Cohttp_async.Body.t)) ->
-      let status = Cohttp.Response.status response in
-      let code = Cohttp.Code.code_of_status status in
-      Log.Global.error_s
-        [%message
-          "Received error response" (code : int) (status : Cohttp.Code.status_code)];
-      let backoff = Backoff.increment backoff in
-      let%bind () = Backoff.after backoff in
-      loop before backoff cache_busting_counter
+    | Error response ->
+      (match%bind on_error state response with
+      | Stop result -> return result
+      | Continue state ->
+        let backoff = Backoff.increment backoff in
+        let%bind () = Backoff.after backoff in
+        loop state before backoff cache_busting_counter)
     | Ok list ->
       let list =
         List.rev list
         |> List.filter ~f:(fun child ->
                not (Bounded_set.mem seen (get_before_parameter child : id)))
       in
-      let%bind () =
-        Deferred.List.iter list ~f:(fun child ->
-            Bounded_set.add seen (get_before_parameter child);
-            match first_pass with
-            | true -> return ()
-            | false -> f child)
+        List.iter list ~f:(fun child -> Bounded_set.add seen (get_before_parameter child));
+      let continue state =
+        match List.hd list with
+        | None ->
+          let backoff = Backoff.increment backoff in
+          let%bind () = Backoff.after backoff in
+          loop state None backoff cache_busting_counter
+        | Some child ->
+          let before = Some (get_before_parameter child) in
+          let backoff = Backoff.initial in
+          let%bind () = Backoff.after backoff in
+          loop state before backoff cache_busting_counter
       in
-      (match List.hd list with
-      | None ->
-        let backoff = Backoff.increment backoff in
-        let%bind () = Backoff.after backoff in
-        loop None backoff cache_busting_counter
-      | Some child ->
-        let before = Some (get_before_parameter child) in
-        let backoff = Backoff.initial in
-        let%bind () = Backoff.after backoff in
-        loop before backoff cache_busting_counter)
+      (match first_pass with
+      | true -> continue state
+      | false ->
+        (match%bind
+           fold_helper list ~init:state ~f
+         with
+        | Stop result -> return result
+        | Continue state -> continue state))
   in
-  loop ~first_pass:true None Backoff.initial 0
+  loop ~first_pass:true init None Backoff.initial 0
+;;
+
+let fold
+    (type id)
+    (module Id : Hashable.S with type t = id)
+    connection
+    ~get_listing
+    ~get_before_parameter
+    ~init
+    ~f
+    ~on_error
+  =
+  let continue_after state x ~f =
+    let%bind state = f state x in
+    return (Continue state)
+  in
+  fold_until_finished
+    (module Id)
+    connection
+    ~get_listing
+    ~get_before_parameter
+    ~init
+    ~f:(continue_after ~f)
+    ~on_error:(continue_after ~f:on_error)
+;;
+
+let iter
+    (type id)
+    (module Id : Hashable.S with type t = id)
+    connection
+    ~get_listing
+    ~get_before_parameter
+    ~f
+  =
+  fold
+    (module Id)
+    connection
+    ~get_listing
+    ~get_before_parameter
+    ~init:()
+    ~f:(fun () child -> f child)
+    ~on_error:(fun () (response, (_ : Cohttp_async.Body.t)) ->
+      let status = Cohttp.Response.status response in
+      let code = Cohttp.Code.code_of_status status in
+      Log.Global.error_s
+        [%message
+          "Received error response" (code : int) (status : Cohttp.Code.status_code)];
+      return ())
 ;;
--- src/stream.mli
+++ src/stream.mli
@@ -11,4 +11,35 @@ val iter
         -> ('thing list, Cohttp.Response.t * Cohttp_async.Body.t) result Deferred.t)
   -> get_before_parameter:('thing -> 'id)
   -> f:('thing -> unit Deferred.t)
-  -> 'b Deferred.t
+  -> _ Deferred.t
+
+val fold
+  :  (module Hashable.S with type t = 'id)
+  -> Connection.t
+  -> get_listing:
+       (Connection.t
+        -> before:'id option
+        -> limit:int
+        -> ('thing list, Cohttp.Response.t * Cohttp_async.Body.t) result Deferred.t)
+  -> get_before_parameter:('thing -> 'id)
+  -> init:'state
+  -> f:('state -> 'thing -> 'state Deferred.t)
+  -> on_error:('state -> Cohttp.Response.t * Cohttp_async.Body.t -> 'state Deferred.t)
+  -> _ Deferred.t
+
+val fold_until_finished
+  :  (module Hashable.S with type t = 'id)
+  -> Connection.t
+  -> get_listing:
+       (Connection.t
+        -> before:'id option
+        -> limit:int
+        -> ('thing list, Cohttp.Response.t * Cohttp_async.Body.t) result Deferred.t)
+  -> get_before_parameter:('thing -> 'id)
+  -> init:'state
+  -> f:('state -> 'thing -> ('state, 'result) Continue_or_stop.t Deferred.t)
+  -> on_error:
+       ('state
+        -> Cohttp.Response.t * Cohttp_async.Body.t
+        -> ('state, 'result) Continue_or_stop.t Deferred.t)
+  -> 'result Deferred.t
