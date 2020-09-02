e605daf824a001b58defa5d0010ace662f689e96
Levi Roth <levimroth@gmail.com>
2020-09-02 08:10:44 -0400

    Ocamlformat test_user_fields.ml

--- test/test_user_fields.ml
+++ test/test_user_fields.ml
@@ -8,21 +8,21 @@ let%expect_test "user_fields" =
         Api.Exn.about_user ~username:(Username.of_string "spez") connection
       in
       print_s [%sexp (Thing.User.name user : Username.t)];
-      let%bind () = [%expect{| spez |}] in
+      let%bind () = [%expect {| spez |}] in
       print_s [%sexp (Thing.User.creation_time user : Time_ns.t)];
-      let%bind () = [%expect{| (2005-06-06 04:00:00.000000000Z) |}] in
+      let%bind () = [%expect {| (2005-06-06 04:00:00.000000000Z) |}] in
       print_s [%sexp (Thing.User.link_karma user : int)];
-      let%bind () = [%expect{| 138988 |}] in
+      let%bind () = [%expect {| 138988 |}] in
       print_s [%sexp (Thing.User.comment_karma user : int)];
-      let%bind () = [%expect{| 743899 |}] in
+      let%bind () = [%expect {| 743899 |}] in
       print_s [%sexp (Thing.User.awarder_karma user : int)];
-      let%bind () = [%expect{| 625 |}] in
+      let%bind () = [%expect {| 625 |}] in
       print_s [%sexp (Thing.User.awardee_karma user : int)];
-      let%bind () = [%expect{| 62329 |}] in
+      let%bind () = [%expect {| 62329 |}] in
       print_s [%sexp (Thing.User.total_karma user : int)];
-      let%bind () = [%expect{| 945841 |}] in
+      let%bind () = [%expect {| 945841 |}] in
       print_s
         [%sexp (Thing.User.subreddit user |> Thing.Subreddit.name : Subreddit_name.t)];
-      let%bind () = [%expect{| u_spez |}] in
+      let%bind () = [%expect {| u_spez |}] in
       return ())
 ;;
