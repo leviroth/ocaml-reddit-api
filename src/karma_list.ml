c413977d98c001e04a405e05521ee987bfebd8fa
Levi Roth <levimroth@gmail.com>
2020-09-14 19:58:00 -0400

    Use Json_object_utils for Karma_list

--- src/karma_list.ml
+++ src/karma_list.ml
@@ -1,34 +1,18 @@
 open! Core
 
 module Entry = struct
-  type t =
-    { subreddit : Subreddit_name.t
-    ; comment_karma : int
-    ; link_karma : int
-    }
-  [@@deriving sexp]
+  include Json_object_utils
 
   let of_json json =
     match json with
-    | `Object
-        [ ("sr", `String subreddit_name)
-        ; ("comment_karma", `Number comment_karma)
-        ; ("link_karma", `Number link_karma)
-        ] ->
-      { subreddit = Subreddit_name.of_string subreddit_name
-      ; comment_karma = Int.of_string comment_karma
-      ; link_karma = Int.of_string link_karma
-      }
+    | `Object alist -> String.Map.of_alist_exn alist
     | _ -> raise_s [%message "Invalid [Karma_list.Entry] JSON" (json : Json.t)]
   ;;
 
-  let to_json { subreddit; comment_karma; link_karma } =
-    `Object
-      [ "sr", `String (Subreddit_name.to_string subreddit)
-      ; "comment_karma", `Number (Int.to_string comment_karma)
-      ; "link_karma", `Number (Int.to_string link_karma)
-      ]
-  ;;
+  let to_json t = `Object (Map.to_alist t)
+  let subreddit = required_field "sr" subreddit_name
+  let link_karma = required_field "link_karma" int
+  let comment_karma = required_field "comment_karma" int
 end
 
 type t = Entry.t list [@@deriving sexp]
