0ee711ede75239f89cb72f6ec0ff005a32215073
Levi Roth <levimroth@gmail.com>
2021-01-09 20:47:02 -0500

    Add Api.user_trophies to view trophies of non-logged-in users

--- test/test_trophies.ml
+++ test/test_trophies.ml
@@ -21,3 +21,46 @@ let%expect_test _ =
            (id (String 1qr5eq)) (name (String "Verified Email")) (url Null)))) |}];
       return ())
 ;;
+
+let%expect_test _ =
+  with_cassette "user_trophies" ~f:(fun connection ->
+      let%bind trophies =
+        Connection.call_exn
+          connection
+          (Api.user_trophies ~username:(Username.of_string "spez") ())
+      in
+      print_s [%message "" ~trophies:(List.take trophies 5 : Thing.Award.t list)];
+      [%expect
+        {|
+        (trophies
+         (((award_id Null) (description Null) (granted_at (Float 1591416000))
+           (icon_40
+            (String https://www.redditstatic.com/awards2/15_year_club-40.png))
+           (icon_70
+            (String https://www.redditstatic.com/awards2/15_year_club-70.png))
+           (id Null) (name (String "15-Year Club")) (url Null))
+          ((award_id (String 33)) (description Null) (granted_at Null)
+           (icon_40 (String https://www.redditstatic.com/awards2/Argentium-40.png))
+           (icon_70 (String https://www.redditstatic.com/awards2/Argentium-70.png))
+           (id (String 2mimqo)) (name (String "Argentium Club")) (url Null))
+          ((award_id (String 32)) (description Null) (granted_at Null)
+           (icon_40
+            (String https://www.redditstatic.com/awards2/Wearing-is-Caring-40.png))
+           (icon_70
+            (String https://www.redditstatic.com/awards2/Wearing-is-Caring-70.png))
+           (id (String 2migfn)) (name (String "Wearing is Caring")) (url Null))
+          ((award_id (String 31)) (description Null) (granted_at Null)
+           (icon_40 (String https://www.redditstatic.com/awards2/100-awards-40.png))
+           (icon_70 (String https://www.redditstatic.com/awards2/100-awards-70.png))
+           (id (String 2m8tiw)) (name (String "100 Awards Club")) (url Null))
+          ((award_id (String 8)) (description (String 2020-06-05)) (granted_at Null)
+           (icon_40
+            (String https://www.redditstatic.com/awards2/inciteful_comment-40.png))
+           (icon_70
+            (String https://www.redditstatic.com/awards2/inciteful_comment-70.png))
+           (id (String 2houdv)) (name (String "Inciteful Comment"))
+           (url
+            (String
+             /r/announcements/comments/gxas21/upcoming_changes_to_our_content_policy_our_board/ft0ekhk/?context=5#ft0ekhk))))) |}];
+      return ())
+;;
