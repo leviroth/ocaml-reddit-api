da00b3de8f4986bc37fd433e3c4722492fe30c3c
Levi Roth <levimroth@gmail.com>
2021-07-25 09:50:07 -0400

    Test showing bad errors on insufficient scope

--- test/test_oauth2_refresh_token.ml
+++ test/test_oauth2_refresh_token.ml
@@ -29,3 +29,52 @@ let%expect_test "oauth2_refresh_token" =
       [%expect {| ((id odlsl2) (title test)) |}];
       return ())
 ;;
+
+let%expect_test "oauth2_refresh_token_insufficient_scope" =
+  with_cassette "oauth2_refresh_token_insufficient_scope" ~f:(fun connection ->
+      let%bind link = get_link_exn connection "odlsl2" in
+      print_s
+        [%sexp
+          { id : Thing.Link.Id.t = Thing.Link.id link
+          ; title : string = Thing.Link.title link
+          }];
+      return ())
+[@@expect.uncaught_exn {|
+  (* CR expect_test_collector: This test expectation appears to contain a backtrace.
+     This is strongly discouraged as backtraces are fragile.
+     Please change this test to not include a backtrace. *)
+
+  (monitor.ml.Error
+    (Cohttp_raised
+      (monitor.ml.Error
+        ("Missing expected X-Ratelimit header" (header X-Ratelimit-Remaining)
+          (headers
+            ((accept-ranges bytes) (access-control-allow-origin *)
+              (access-control-expose-headers X-Moose)
+              (cache-control "max-age=0, must-revalidate")
+              (connection keep-alive) (content-length 38)
+              (content-type "application/json; charset=UTF-8")
+              (date "Sun, 25 Jul 2021 13:48:26 GMT") (server snooserv)
+              (set-cookie
+                "edgebucket=DOY2vtwecNrBUXjM3E; Domain=reddit.com; Max-Age=63071999; Path=/;  secure")
+              (set-cookie
+                "csv=1; Max-Age=63072000; Domain=.reddit.com; Path=/; Secure; SameSite=None")
+              (set-cookie
+                "session_tracker=GiIdHGp9KRBNycfn5d.0.1627220906977.Z0FBQUFBQmdfV3VxLUtmQ04tRzlVQlQ4dVVGNFVRWGx0Rm1qX2tqMzlYMERCTWZCWTR6bnV1Wk1na3ladXJMNmo5a1BxQnU4VHZ0aGNwT0RWbEE1NFcwWDdZV3VLMWx5Qlp6a3hJSlhfdXYyOUVMUjlUNlY0N3B3d3JaazBRbmhjS0NncVM1dERtOXI; Domain=reddit.com; Max-Age=7199; Path=/; expires=Sun, 25-Jul-2021 15:48:26 GMT; secure; SameSite=None; Secure")
+              (set-cookie
+                "loid=0000000000dijrnrio.2.1627220906977.Z0FBQUFBQmdfV3VxY0tER3dTQXZyakFWdnpuSjJFcW1jNmhLeG9CWWNUcHpXMXVTMlpNNXA5YWtYTjJoLWIwNWRDcU5tNFluTkF1cDZRZFZaRElPYzZNdjN6c2VFS01RbkFZMGpsa3AyLTZidElfRlJ1TkpKN2Nzdi1hUG8ycnIwUUxpUlB5eDRCZXg; Domain=reddit.com; Max-Age=63071999; Path=/; expires=Tue, 25-Jul-2023 13:48:26 GMT; secure; SameSite=None; Secure")
+              (strict-transport-security
+                "max-age=15552000; includeSubDomains; preload")
+              (via "1.1 varnish")
+              (www-authenticate
+                "Bearer realm=\"reddit\", error=\"insufficient_scope\"")
+              (x-clacks-overhead "GNU Terry Pratchett")
+              (x-content-type-options nosniff) (x-frame-options SAMEORIGIN)
+              (x-moose majestic) (x-ua-compatible IE=edge)
+              (x-xss-protection "1; mode=block"))))
+        ("<backtrace elided in test>")))
+    ("<backtrace elided in test>" "Caught by monitor Monitor.protect"))
+  Raised at Base__Result.ok_exn in file "src/result.ml" (inlined), line 201, characters 17-26
+  Called from Async_unix__Thread_safe.block_on_async_exn in file "src/thread_safe.ml", line 132, characters 29-63
+  Called from Expect_test_collector.Make.Instance.exec in file "collector/expect_test_collector.ml", line 244, characters 12-19 |}]
+;;
