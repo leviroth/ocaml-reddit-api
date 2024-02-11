60030813800443a7038830701cf8bf3298d4dcd6
Levi Roth <levimroth@gmail.com>
2024-02-11 16:49:15 -0500

    Add a little documentation to synchronous_rate_limiter.mli

--- reddit_api_kernel/synchronous_rate_limiter.mli
+++ reddit_api_kernel/synchronous_rate_limiter.mli
@@ -9,9 +9,20 @@ end
 
 type t [@@deriving sexp_of]
 
+(** {1 Constructors} *)
+
 val by_headers : t
 val with_minimum_delay : delay:Time_ns.Span.t -> t
 val combine : t list -> t
-val wait_until : t -> When_to_send.t
+
+(** {1 Events} *)
+
+(** [sent_request_unchecked] should called immediately after sending a request.
+    It is the caller's responsibility to first call {!wait_until}. *)
 val sent_request_unchecked : t -> now:Time_ns.t -> t
+
 val received_response : t -> Cohttp.Response.t -> t
+
+(** {1 Accessors} *)
+
+val wait_until : t -> When_to_send.t
